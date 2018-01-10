788e2f25265802229888322f0cec0e5f66281d59
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-01-10 09:35:50 +0100

    testing host detection in CMAKE

--- lib/ASPENConfig.cmake
+++ lib/ASPENConfig.cmake
@@ -1,3 +1,14 @@
+
+cmake_host_system_information(RESULT _host_name QUERY HOSTNAME)
+message("-- _host_name variable is set to: " ${_host_name})
+
+
+if ( ${_host_name} STREQUAL "fortuna")
+	message("THIS IS FORTUNA")
+
+endif()
+
+
 set(ASPEN_INCLUDE_HEADER /usr/local/include/ASPEN)
 set(ASPEN_INCLUDE_GNUPLOT /usr/local/include/ASPEN/external/Gnuplot)
 set(ASPEN_INCLUDE_CGAL /usr/local/include/ASPEN/external/CGAL_interface)
@@ -11,5 +22,8 @@ elseif(UNIX AND NOT APPLE)
 else()
 	message(FATAL_ERROR "Unsupported platform")
 endif()
+
+
+
 
 message("-- Found ASPEN: " ${ASPEN_LIBRARY})
