866bf210c11128d23759500828beb3be5edb5920
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-02-28 16:06:07 -0700

    edited TestBezier

--- Apps/TestBezier/main.cpp
+++ Apps/TestBezier/main.cpp
@@ -15,17 +15,25 @@ int main(){
 	ShapeModelBezier test("", &frame_graph);
 
 
-	ShapeModelImporter shape_io("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/shape_model/fit_source_400.b", 1, true);
-	ShapeModelImporter shape_io_tri("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/shape_model/fit_source_400.obj", 1, true);
-		
+	// ShapeModelImporter shape_io("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/shape_model/fit_source_400.b", 1, true);
+	// ShapeModelImporter shape_io_tri("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/shape_model/fit_source_400.obj", 1, true);
+	
 
-	// ShapeModelImporter shape_io("../input/cube.b", 1, true);
-	// ShapeModelImporter shape_io_tri("../input/cube.obj", 1, true);
+	ShapeModelImporter shape_io("../input/cube.b", 1, true);
+	ShapeModelImporter shape_io_tri("../input/cube.obj", 1, true);
 	
 
 	shape_io.load_bezier_shape_model(&test);
 	shape_io_tri.load_obj_shape_model(&test_tri);
+
+	test.elevate_degree();
+	test.elevate_degree();
+	test.elevate_degree();
+	test.elevate_degree();
+	test.elevate_degree();
+	test.elevate_degree();
 	
+
 	std::cout << "\nVolume: \n";
 
 	std::cout << test.get_volume() << std::endl;
