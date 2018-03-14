87b2e0475e3024d4bb67d54e351dd3c0070b6f9e
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-03-14 13:43:52 -0600

    removed parallel stuff from Batch and turned on proportional noise

--- lib/source/BatchFilter.cpp
+++ lib/source/BatchFilter.cpp
@@ -159,9 +159,9 @@ int  BatchFilter::run(
 			else{
 				mes_range = true_ranges(p);
 				W(p,p) = 1./(std::pow(args.get_sd_noise() + args.get_sd_noise_prop() * mes_range,2));
-				std::cout << W(p,p) << std::endl;
-				
-				
+				std::cout << W(p,p) << " " << mes_range << std::endl;
+
+
 			}
 			
 			biases(p) = biases_consider_vector_ptr[p];
