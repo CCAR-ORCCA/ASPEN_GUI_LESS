f1946a4ed1a8dc46709c57385d673c610fb2cf74
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-03-28 11:07:57 -0600

    testing

--- Apps/TestBezier/main.cpp
+++ Apps/TestBezier/main.cpp
@@ -21,7 +21,7 @@ int main(){
 	ShapeModelBezier itokawa_bezier_fit("", &frame_graph);
 
 
-	ShapeModelImporter shape_io_bezier("../../../resources/shape_models/cube.b", 1, true);
+	ShapeModelImporter shape_io_bezier("../../../resources/shape_models/cube_2.b", 1, true);
 	ShapeModelImporter shape_io_true("../../../resources/shape_models/cube.obj", 1, true);
 	
 	// ShapeModelImporter shape_io_bezier("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/shape_model/test/fit_shape_aligned.b", 1, true);
@@ -97,7 +97,7 @@ int main(){
 
 	arma::vec cm_mean = itokawa_bezier_fit.get_center_of_mass();
 	arma::mat cm_cov = itokawa_bezier_fit.get_cm_cov();
-	throw;
+	// throw;
 
 
 	std::cout << "\nVolume standard deviation: " << std::endl;
