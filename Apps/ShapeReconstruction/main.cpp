1ad7e969a330bf6ae4aa91301ebb23e675c84430
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-01 20:15:43 -0600

    running simulation

--- Apps/ShapeReconstruction/main.cpp
+++ Apps/ShapeReconstruction/main.cpp
@@ -69,7 +69,7 @@
 #define DENSITY 1900
 
 // Use ICP (false if point cloud is generated from true shape)
-#define USE_ICP true
+#define USE_ICP false
 
 ///////////////////////////////////////////
 
