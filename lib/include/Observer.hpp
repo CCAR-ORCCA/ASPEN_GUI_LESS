56c593f6c94b464c0052105674bad42e753e9065
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-13 11:10:17 -0600

    added stm switching

--- lib/include/Observer.hpp
+++ lib/include/Observer.hpp
@@ -60,7 +60,20 @@ push_back_augmented_state( std::vector< arma::vec > & states ) : m_states( state
 void operator()( arma::vec & x,  double t){
 
 	if (arma::norm(x.subvec(6,8)) > 1){
-		x.subvec(6,8) = - x.subvec(6,8) / arma::dot(x.subvec(6,8),x.subvec(6,8));
+
+		// The state is switched
+		arma::vec sigma = x.subvec(6,8);
+		x.subvec(6,8) = - sigma / arma::dot(sigma,sigma);
+
+		// Switching matrix
+		arma::mat Theta = arma::eye<arma::mat>(12,12);
+		Theta.submat(6,6,8,8) = 1./(arma::dot(sigma,sigma)) * (2 * sigma * sigma.t() / arma::dot(sigma,sigma) - arma::eye<arma::mat>(3,3));
+
+		arma::mat switched_stm = Theta * arma::reshape(x.rows(12,12 + 12 * 12 - 1),12,12) * Theta.t();
+
+		// The stm is switched
+		x.rows(12,12 + 12 * 12 - 1) = arma::vectorise(switched_stm);
+
 	}
 	
 	m_states.push_back( x );
