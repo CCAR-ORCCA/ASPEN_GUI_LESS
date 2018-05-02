c8c70a211381287d057ed59300ca83314ff29d77
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-01 21:27:16 -0600

    fixed a few bugs

--- lib/source/FlyOverMap.cpp
+++ lib/source/FlyOverMap.cpp
@@ -45,11 +45,14 @@ void FlyOverMap::get_flyovers_in_bin(int bin_longitude,int bin_latitude,
 
 	while(pc_in_bin.size() > 1){
 		for (int pc_index = 0; pc_index < pc_in_bin.size() - 1; ++pc_index){
-			std::set<int> new_pair;
-			new_pair.insert(pc_in_bin.back());
-			new_pair.insert(pc_in_bin[pc_index]);
 
-			flyovers.insert(new_pair);
+			if (std::abs(pc_in_bin.back() - pc_in_bin[pc_index]) != 1){
+				std::set<int> new_pair;
+				new_pair.insert(pc_in_bin.back());
+				new_pair.insert(pc_in_bin[pc_index]);
+
+				flyovers.insert(new_pair);
+			}
 
 		}
 		pc_in_bin.pop_back();
@@ -61,7 +64,11 @@ bool FlyOverMap::has_flyovers(double longitude, double latitude) const {
 
 	int bin_longitude = int(longitude / this -> d_bin_longitude) + this -> n_bins_longitude/2;
 	int bin_latitude = int(latitude / this -> d_bin_latitude) + this -> n_bins_latitude/2;
-	return (this -> get_bin_depth(bin_longitude,bin_latitude ) > 1) ;
+	
+	std::set<std::set<int> > & flyovers;
+	this -> get_flyovers_in_bin(bin_longitude,bin_latitude,flyovers );
+
+	return (flyovers.size() > 0) ;
 }
 
 
