2b44b7fd79885250171c65e55a0b88372bd932a4
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-05 17:05:44 -0600

    testing

--- lib/source/ShapeBuilder.cpp
+++ lib/source/ShapeBuilder.cpp
@@ -162,14 +162,7 @@ void ShapeBuilder::run_shape_reconstruction(const arma::vec &times ,
 
 				if (rigid_transforms.size() == 6){
 
-					arma::vec true_particle(7);
-
-					true_particle.subvec(0,5) = this -> true_kep_state_t0.get_state();
-
-					true_particle(6) = this -> true_kep_state_t0.get_mu();
-
-					std::cout << " Evaluating the cost function at the true state: " << IODFinder::cost_function(true_particle,&rigid_transforms) << std::endl;
-
+					
 
 					IODFinder iod_finder(&rigid_transforms, 
 						100, 
@@ -185,6 +178,13 @@ void ShapeBuilder::run_shape_reconstruction(const arma::vec &times ,
 
 					iod_finder.run(lower_bounds,upper_bounds);
 					OC::KepState estimated_state = iod_finder.get_result();
+					arma::vec true_particle(7);
+
+					true_particle.subvec(0,5) = this -> true_kep_state_t0.get_state();
+
+					true_particle(6) = this -> true_kep_state_t0.get_mu();
+
+					std::cout << " Evaluating the cost function at the true state: " << IODFinder::cost_function(true_particle,&rigid_transforms) << std::endl;
 
 					std::cout << " True keplerian state at epoch: \n" << this -> true_kep_state_t0.get_state() << " with mu :" << this -> true_kep_state_t0.get_mu() << std::endl;
 					std::cout << " Estimated keplerian state at epoch: \n" << estimated_state.get_state() << " with mu :" << estimated_state.get_mu() << std::endl;
