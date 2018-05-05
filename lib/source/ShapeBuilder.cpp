44e308df93ae30da51b960fb373cdbca0218246a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-05 15:50:55 -0600

    checking cost function

--- lib/source/ShapeBuilder.cpp
+++ lib/source/ShapeBuilder.cpp
@@ -200,7 +200,11 @@ void ShapeBuilder::run_shape_reconstruction(const arma::vec &times ,
 
 					std::cout << " True keplerian state at epoch: " << this -> true_kep_state_t0.get_state() << " with mu :" << this -> true_kep_state_t0.get_mu() << std::endl;
 					std::cout << " Estimated keplerian state at epoch: " << estimated_state.get_state() << " with mu :" << estimated_state.get_mu() << std::endl;
-					
+					arma::vec true_particle(7);
+					true_particle.subvec(0,5) = this -> true_kep_state_t0.get_state();
+					true_particle.subvec(6) = this -> true_kep_state_t0.get_mu();
+
+					std::cout << "evaluting the cost function at the true state: " << iod_finder::cost_function(true_particle,rigid_transforms) << std::endl;
 
 					throw;
 
