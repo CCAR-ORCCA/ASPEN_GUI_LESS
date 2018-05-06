150a23e9e31a39d316547ce9e26ae63371c26080
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-05 22:47:44 -0600

    tessting

--- lib/source/ShapeBuilder.cpp
+++ lib/source/ShapeBuilder.cpp
@@ -171,8 +171,8 @@ void ShapeBuilder::run_shape_reconstruction(const arma::vec &times ,
 					
 
 					IODFinder iod_finder(&rigid_transforms, 
-						1500, 
-						1000,
+						150, 
+						10,
 						true);
 
 					arma::vec true_particle(7);
