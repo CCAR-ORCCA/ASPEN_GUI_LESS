bd321a1adc76ace1b08843dcce953d4289de91c8
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-05 23:03:34 -0600

    tessting

--- lib/source/Psopt.cpp
+++ lib/source/Psopt.cpp
@@ -149,6 +149,7 @@ double tolerance) {
 
 
 			std::cout << arma::norm(local_best(particle) - this -> population.row(particle))<<std::endl;
+			std::cout << this -> population.row(particle) << std::endl;
 			std::cout << local_best(particle);
 			std::cout << velocities.row(particle);
 
