c636f6dd2d40425ec082af3ac86ee711d90ac53c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-05 18:22:18 -0600

    testing

--- lib/source/Psopt.cpp
+++ lib/source/Psopt.cpp
@@ -80,7 +80,7 @@ double tolerance) {
 
 		// #pragma omp parallel for 
 		for (unsigned int particle = 0; particle < this -> population_size; ++particle) {
-
+			std::cout << particle << std::endl;
 			for (unsigned int state_index = 0; state_index < this -> lower_bounds.n_rows; ++state_index) {
 
 				// Boundary check
