14d5016c9d8eb15cc6b69b7da46ac06de6cc39ad
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-07 08:40:09 -0600

    less particles, more iterations

--- Apps/ShapeReconstruction/main.cpp
+++ Apps/ShapeReconstruction/main.cpp
@@ -55,8 +55,8 @@
 
 // IOD parameters
 #define IOD_RIGID_TRANSFORMS_NUMBER 30 // Number of rigid transforms to be used in each IOD run
-#define IOD_PARTICLES 300000 // Number of particles
-#define IOD_ITERATIONS 400 // Number of iteraions
+#define IOD_PARTICLES 1000 // Number of particles
+#define IOD_ITERATIONS 100000 // Number of iteraions
 
 
 // Target properties
