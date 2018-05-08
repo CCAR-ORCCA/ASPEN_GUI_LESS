dc605e3e2e810b1dff394e7f155bb3bc2cfc5700
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-08 10:57:32 -0600

    sync

--- Apps/ShapeReconstruction/main.cpp
+++ Apps/ShapeReconstruction/main.cpp
@@ -55,7 +55,7 @@
 
 // IOD parameters
 #define IOD_RIGID_TRANSFORMS_NUMBER 30 // Number of rigid transforms to be used in each IOD run
-#define IOD_PARTICLES 300 // Number of particles
+#define IOD_PARTICLES 10000 // Number of particles
 #define IOD_ITERATIONS 30000 // Number of iterations
 
 // Target properties
