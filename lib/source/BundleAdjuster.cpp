71c2c9134f9f42fa8fd29513a00f417e462d01d8
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-11 17:37:56 -0600

    test

--- lib/source/BundleAdjuster.cpp
+++ lib/source/BundleAdjuster.cpp
@@ -325,7 +325,7 @@ void BundleAdjuster::create_pairs( bool look_for_closure){
 
 	// Only the point cloud pairs that "close the loop" with the ground point cloud are kep
 	std::vector<int> local_pc_index_to_global_pc_index_temp;
-	for (int i = 0; i < this -> cutoff_index; ++i){
+	for (int i = 0; i <= this -> cutoff_index; ++i){
 		local_pc_index_to_global_pc_index_temp.push_back(this -> local_pc_index_to_global_pc_index[i]);
 		std::cout << "Keeping point cloud " << this -> all_registered_pc -> at(local_pc_index_to_global_pc_index_temp.back()) -> get_label() << std::endl;
 	}
