48bdc0dca1e67c7e10353ea313ed94822418030e
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-11 20:52:37 -0600

    some simplifications

--- lib/source/BundleAdjuster.cpp
+++ lib/source/BundleAdjuster.cpp
@@ -58,9 +58,9 @@ BundleAdjuster::BundleAdjuster(
 	
 
 	// The connectivity matrix is saved
-	// if (save_connectivity){
-	this -> save_connectivity();
-	// }
+	if (save_connectivity){
+		this -> save_connectivity();
+	}
 }
 
 
