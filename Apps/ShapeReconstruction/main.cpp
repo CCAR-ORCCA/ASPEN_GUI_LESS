3c1b7375fdfa2b05e48ca77d62ba80f5a30e4fc7
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-16 14:47:44 -0600

    larger a-priori covariance

--- Apps/ShapeReconstruction/main.cpp
+++ Apps/ShapeReconstruction/main.cpp
@@ -72,6 +72,12 @@
 #define N_ITER_MES_UPDATE 10 // Number of iterations in the navigation filter measurement update
 #define USE_CONSISTENCY_TEST false // If true, will exit IEKF if consistency test is satisfied
 
+// A-priori covariance
+#define SIGMA_POS 10 // a-priori sd on position (m)
+#define SIGMA_VEL 1e-2 // a-priori sd on velocity (m/s)
+#define SIGMA_MRP 1e-3 // a-priori sd on mrp (-)
+#define SIGMA_OMEGA 1e-5 // a-priori sd on angular velocity (rad/s)
+
 // CHEATS (true: cheat is disabled)
 #define USE_BA true // Whether or not the bundle adjustment should be used
 #define USE_ICP true // Use ICP (false if point cloud is generated from true shape)
@@ -364,10 +370,10 @@ int main() {
 
 	// A-priori covariance on spacecraft state and asteroid state.
 	arma::vec P0_diag = {
-		100,100,100,//position
-		1e-5,1e-5,1e-5,//velocity
-		1e-3,1e-3,1e-3,// mrp
-		1e-10,1e-10,1e-10 // angular velocity
+		SIGMA_POS * SIGMA_POS,SIGMA_POS * SIGMA_POS,SIGMA_POS * SIGMA_POS,//position
+		SIGMA_VEL * SIGMA_VEL,SIGMA_VEL * SIGMA_VEL,SIGMA_VEL * SIGMA_VEL,//velocity
+		SIGMA_MRP * SIGMA_MRP,SIGMA_MRP * SIGMA_MRP,SIGMA_MRP * SIGMA_MRP,// mrp
+		SIGMA_OMEGA * SIGMA_OMEGA,SIGMA_OMEGA * SIGMA_OMEGA,SIGMA_OMEGA * SIGMA_OMEGA // angular velocity
 	};
 
 	arma::mat P0 = arma::diagmat(P0_diag);
