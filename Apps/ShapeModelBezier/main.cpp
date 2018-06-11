526082c0812b5d275a6e616acb01ea3124f39079
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-06-10 20:45:35 -0600

    investigating

--- Apps/ShapeModelBezier/main.cpp
+++ Apps/ShapeModelBezier/main.cpp
@@ -14,31 +14,41 @@
 int main(){
 	FrameGraph frame_graph;
 
-
+	ShapeModelTri a_priori_obj("", nullptr);
 
 	#ifdef __APPLE__
-
-
-
-	ShapeModelImporter shape_io_guess("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/test/apriori.obj", 1, true);
 	PC pc("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/pc/source_transformed_poisson.obj");
+	
+	ShapeModelImporter shape_io_guess("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/test/apriori.obj", 1, true);
+	
+	// ShapeModelImporter shape_io_guess("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/resources/shape_models/cube.obj", 1, true);
+	shape_io_guess.load_obj_shape_model(&a_priori_obj);
+
+	// arma::mat points,normals;
+	// a_priori_obj.random_sampling(30,points, normals);
+	// PC pc(points,normals);
 
 	#elif __linux__
+
+	// ShapeModelImporter shape_io_guess("/home/bebe0705/libs/ASPEN_GUI_LESS/Apps/ShapeReconstruction/output/shape_model/apriori.obj", 1, true);
+	// PC pc("/home/bebe0705/libs/ASPEN_GUI_LESS/Apps/ShapeReconstruction/output/pc/source_transformed_poisson.obj");
 	
-	ShapeModelImporter shape_io_guess("/home/bebe0705/libs/ASPEN_GUI_LESS/Apps/ShapeReconstruction/output/shape_model/apriori.obj", 1, true);
-	PC pc("/home/bebe0705/libs/ASPEN_GUI_LESS/Apps/ShapeReconstruction/output/pc/source_transformed_poisson.obj");
+	ShapeModelImporter shape_io_guess("/home/bebe0705/libs/ASPEN_GUI_LESS/resources/shape_models/cube.obj", 1, true);
+	shape_io_guess.load_obj_shape_model(&a_priori_obj);
+
+	// arma::mat points,normals;
+	// a_priori_obj.random_sampling(30,points, normals);
+	// PC pc(points,normals);
 
 	#endif
 
-	ShapeModelTri a_priori_obj("", nullptr);
 
-	shape_io_guess.load_obj_shape_model(&a_priori_obj);
 
 	std::shared_ptr<ShapeModelBezier> a_priori_bezier = std::make_shared<ShapeModelBezier>(ShapeModelBezier(&a_priori_obj,"E", &frame_graph));
 
 	// the shape is elevated to the prescribed degree
 	unsigned int starting_degree = a_priori_bezier -> get_degree();
-	a_priori_bezier -> elevate_degree();
+	// a_priori_bezier -> elevate_degree();
 	
 	a_priori_bezier -> initialize_index_table();
 
