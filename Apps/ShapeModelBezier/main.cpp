29f6abf6386bfe0c9fba49c58394133d11bd5196
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-06-10 20:10:02 -0600

    testing

--- Apps/ShapeModelBezier/main.cpp
+++ Apps/ShapeModelBezier/main.cpp
@@ -5,39 +5,46 @@
 #include "ShapeModelTri.hpp"
 #include "ShapeModelBezier.hpp"
 #include "ShapeModelImporter.hpp"
+#include "ShapeFitterBezier.hpp"
+
 
 
 
 #include <chrono>
 int main(){
-
-
 	FrameGraph frame_graph;
+
+
+
+	#ifdef __APPLE__
+
+
+
+	ShapeModelImporter shape_io_guess("/home/bebe0705/libs/ASPEN_gui_less/Apps/ShapeReconstruction/output/shape_model/apriori.obj", 1, true);
+	PC pc("/home/bebe0705/libs/ASPEN_gui_less/Apps/ShapeReconstruction/output/pc/source_transformed_poisson.obj");
+
+	#elif __linux__
+
+	ShapeModelImporter shape_io_guess("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/test/apriori.obj", 1, true);
+	PC pc("/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/ShapeReconstruction/output/pc/source_transformed_poisson.obj");
+
+	#endif
+
+	ShapeModelTri a_priori_obj("", nullptr);
+
+	shape_io_guess.load_obj_shape_model(&a_priori_obj);
+
+	std::shared_ptr<ShapeModelBezier> a_priori_bezier = std::make_shared<ShapeModelBezier>(ShapeModelBezier(&a_priori_obj,"E", &frame_graph));
+
+	// the shape is elevated to the prescribed degree
+	unsigned int starting_degree = a_priori_bezier -> get_degree();
+	a_priori_bezier -> elevate_degree();
 	
-	// Shape model formed with triangles
-	ShapeModelTri shape_tri("", &frame_graph);
+	a_priori_bezier -> initialize_index_table();
 
-	
-	ShapeModelImporter shape_io(
-		"/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/resources/shape_models/itokawa_64.obj", 1000, true);
+	ShapeFitterBezier shape_fitter(a_priori_bezier.get(),&pc);
 
-	shape_io.load_shape_model(&shape_tri);
-	shape_tri.save("tri.obj");
-
-	ShapeModelBezier shape_bezier(&shape_tri,
-		"",
-		&frame_graph);
-
-	unsigned int D = 2;
-	for (unsigned int i = 1; i < D + 1; ++i){
-		std::cout << i << std::endl;
-		shape_bezier.elevate_degree();
-	}
-	shape_bezier.save("bezier_" + std::to_string(D)+ ".obj");
-
-	
-
-
+	shape_fitter.fit_shape_batch(5,0);
 
 
 
