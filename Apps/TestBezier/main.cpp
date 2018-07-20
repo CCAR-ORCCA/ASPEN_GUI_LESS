36aad6138ca33479f1ef9740cd55634419e7631c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-07-20 14:05:33 -0600

    adding latest main to index

--- Apps/TestBezier/main.cpp
+++ Apps/TestBezier/main.cpp
@@ -18,20 +18,10 @@ int main(){
 	
 	ShapeModelTri tri_shape("", &frame_graph);
 
-	ShapeModelImporter shape_io_true("../../../resources/shape_models/itokawa_8.obj", 1, false);
+	ShapeModelImporter shape_io_true("../../../resources/shape_models/tetra.obj", 1, false);
 
 	shape_io_true.load_obj_shape_model(&tri_shape);
-	arma::vec mrp = {0.1,0.2,-0.2};
-	arma::mat M = RBK::mrp_to_dcm(mrp);
-	tri_shape.rotate(M);
-	tri_shape.update_mass_properties();	
-
-	
-
 	ShapeModelBezier bezier_shape(&tri_shape,"", &frame_graph);
-
-
-	
 
 	std::cout << "\nVolume: \n";
 
@@ -48,21 +38,17 @@ int main(){
 	std::cout << bezier_shape.get_inertia() << std::endl;
 	std::cout << tri_shape.get_inertia() << std::endl;
 
-	arma::vec sigmas_sq = (0.00300 * 0.00300) * arma::ones<arma::vec>(3);
+	double sigmas_sq = std::pow(3e-3,2)  ;
 	
 	// arma::vec sigmas_sq = 0.01 * arma::ones<arma::vec>(3) * std::cbrt(bezier_shape.get_volume());
-	std::cout << "SD on point coordinates : " << std::sqrt(sigmas_sq(0)) << std::endl;
-
-	arma::mat P = arma::diagmat(sigmas_sq);
-
-	bezier_shape.shift_to_barycenter();
-	bezier_shape.update_mass_properties();
+	std::cout << "SD on point coordinates : " << std::sqrt(sigmas_sq) << std::endl;
 
 
 	for (unsigned int i = 0; i < bezier_shape.get_NControlPoints(); ++i){
-		arma::vec x = arma::randu<arma::vec>(1);
-		double factor = 5 * x(0);
-		bezier_shape.get_control_point(i) -> set_covariance(factor * P );
+		auto mrp = arma::randn<arma::vec>(3);
+		auto dcm = RBK::mrp_to_dcm(mrp);
+		arma::mat P =  sigmas_sq * dcm * arma::diagmat( arma::randu<arma::vec>(3)) * dcm.t();
+		bezier_shape.get_control_point(i) -> set_covariance(P);
 	}
 
 	bezier_shape.compute_volume_sd();
@@ -80,53 +66,90 @@ int main(){
 
 	std::cout << "\n Elapsed time computing the covariance : " << elapsed_seconds.count() << "s\n\n";
 
-	double volume_mean = bezier_shape.get_volume();
 	double volume_sd = bezier_shape.get_volume_sd();
 
-	arma::vec cm_mean = bezier_shape.get_center_of_mass();
-	arma::mat cm_cov = bezier_shape.get_cm_cov();
+	auto cm_mean = bezier_shape.get_center_of_mass();
+	auto cm_cov = bezier_shape.get_cm_cov();
+	auto inertia_cov = bezier_shape.get_inertia_cov();
+	auto moments_cov = bezier_shape.get_P_moments();
+	auto mrp_cov = bezier_shape.get_mrp_cov();
+	auto Y_cov = bezier_shape.get_P_Y();
+	auto lambda_I_cov = bezier_shape.get_P_lambda_I(2);
+	auto eigenvectors_cov = bezier_shape.get_P_eigenvectors();
+	auto Evectors_cov = bezier_shape.get_P_Evectors();
+	auto MI_cov = bezier_shape.get_P_MI();
 
-	arma::mat inertia_cov = bezier_shape.get_inertia_cov();
-	arma::mat moments_cov = bezier_shape.get_P_moments();
-	// throw;
 
-
-	std::cout << "\nVolume standard deviation: " << std::endl;
-	std::cout << bezier_shape.get_volume_sd() << std::endl;
-
-	std::cout << "\nCenter of mass Covariance: " << std::endl;
-	std::cout << cm_cov << std::endl;
 
 	std::cout << "\nRunning Monte Carlo: " << std::endl;
 
 	int N = 50000;
 
 	arma::vec results_volume;
-	arma::mat results_cm,results_inertia,results_moments;
-	bezier_shape.run_monte_carlo(N,results_volume,results_cm,results_inertia,results_moments);
+	arma::mat results_cm,results_inertia,results_moments,
+	results_mrp,results_lambda_I,results_eigenvectors,results_Evectors,results_Y,results_MI;
 	
-
+	bezier_shape.run_monte_carlo(N,
+		results_volume,
+		results_cm,
+		results_inertia,
+		results_moments,
+		results_mrp,
+		results_lambda_I,
+		results_eigenvectors,
+		results_Evectors,
+		results_Y,
+		results_MI);
+	
 	arma::vec results_cm_mean = arma::mean(results_cm,1);
 	arma::vec results_inertia_mean = arma::mean(results_inertia,1);
 	arma::vec results_moments_mean = arma::mean(results_moments,1);
+	arma::vec results_mrp_mean = arma::mean(results_mrp,1);
+	arma::vec results_lambda_I_mean = arma::mean(results_lambda_I,1);
+	arma::vec results_eigenvectors_mean = arma::mean(results_eigenvectors,1);
+	arma::vec results_Evectors_mean = arma::mean(results_Evectors,1);
+	arma::vec results_Y_mean = arma::mean(results_Y,1);
+	arma::vec results_MI_mean = arma::mean(results_MI,1);
+
 
 	arma::mat cov_cm_mc = arma::zeros(3,3);
 	arma::mat cov_inertia_mc = arma::zeros(6,6);
 	arma::mat cov_moments_mc = arma::zeros(4,4);
-
+	arma::mat cov_mrp_mc = arma::zeros(3,3);
+	arma::vec cov_lambda_I_mc = arma::zeros<arma::vec>(6);
+	arma::mat cov_eigenvectors_mc = arma::zeros<arma::mat>(9,9);
+	arma::mat cov_Evectors_mc = arma::zeros<arma::mat>(9,9);
+	arma::mat cov_Y_mc = arma::zeros<arma::mat>(4,4);
+	arma::vec cov_MI_mc = arma::zeros<arma::vec>(6);
 
 	
 	for (unsigned int i = 0; i < results_cm.n_cols; ++i){
 		cov_cm_mc +=  (results_cm.col(i) - results_cm_mean) * (results_cm.col(i) - results_cm_mean).t();
 		cov_inertia_mc +=  (results_inertia.col(i) - results_inertia_mean) * (results_inertia.col(i) - results_inertia_mean).t();
 		cov_moments_mc +=  (results_moments.col(i) - results_moments_mean) * (results_moments.col(i) - results_moments_mean).t();
+		cov_mrp_mc +=  (results_mrp.col(i) - results_mrp_mean) * (results_mrp.col(i) - results_mrp_mean).t();
+		cov_lambda_I_mc += (results_lambda_I.col(i).rows(0,5) - results_lambda_I_mean.rows(0,5)) * (results_lambda_I.col(i)(6) - results_lambda_I_mean(6));
 
+		cov_eigenvectors_mc +=  (results_eigenvectors.col(i) - results_eigenvectors_mean) * (results_eigenvectors.col(i) - results_eigenvectors_mean).t();
+		cov_Evectors_mc +=  (results_Evectors.col(i) - results_Evectors_mean) * (results_Evectors.col(i) - results_Evectors_mean).t();
+		cov_Y_mc +=  (results_Y.col(i) - results_Y_mean) * (results_Y.col(i) - results_Y_mean).t();
+
+		cov_MI_mc +=  (results_MI.col(i).rows(0,5) - results_MI_mean.rows(0,5)) * (results_MI.col(i)(6) - results_MI_mean(6));
 
 	}
 
 	cov_cm_mc *= 1./(results_cm.n_cols-1);
 	cov_inertia_mc *= 1./(results_inertia.n_cols-1);
 	cov_moments_mc *= 1./(results_moments.n_cols-1);
+	cov_mrp_mc *= 1./(results_mrp.n_cols-1);
+	cov_lambda_I_mc *= 1./(results_lambda_I.n_cols-1);
+	cov_eigenvectors_mc *= 1./(results_eigenvectors.n_cols-1);
+	cov_Evectors_mc *= 1./(results_Evectors.n_cols-1);
+	cov_Y_mc *= 1./(results_Y.n_cols-1);
+	cov_MI_mc *= 1./(results_MI.n_cols-1);
+
+
+
 
 
 	std::cout << "######***** Volume *****######"<< std::endl;
@@ -153,12 +176,62 @@ int main(){
 	std::cout << "Deviation : " << std::endl << (cov_inertia_mc - inertia_cov)/cov_inertia_mc * 100 << " %" << std::endl << std::endl;
 
 
+
+	std::cout << "######***** MI *****######"<< std::endl;
+
+	std::cout << "Mean MI : " << results_MI_mean.t() << std::endl;
+	std::cout << "Inertia MI covariance from MC: " << std::endl << cov_MI_mc << std::endl;
+	std::cout << "Inertia MI covariance predicted: " << std::endl << MI_cov << std::endl << std::endl;
+	std::cout << "Deviation : " << std::endl << (cov_MI_mc - MI_cov)/cov_MI_mc * 100 << " %" << std::endl << std::endl;
+
+
+	std::cout << "######***** Y *****######"<< std::endl;
+
+	std::cout << "Mean Y : " << results_Y_mean.t() << std::endl;
+	std::cout << "Inertia Y covariance from MC: " << std::endl << cov_Y_mc << std::endl;
+	std::cout << "Inertia Y covariance predicted: " << std::endl << Y_cov << std::endl << std::endl;
+	std::cout << "Deviation : " << std::endl << (cov_Y_mc - Y_cov)/cov_Y_mc * 100 << " %" << std::endl << std::endl;
+
+
 	std::cout << "######***** Moments *****######"<< std::endl;
 
 	std::cout << "Mean moments : " << results_moments_mean.t() << std::endl;
 	std::cout << "Inertia moments covariance from MC: " << std::endl << cov_moments_mc << std::endl;
 	std::cout << "Inertia moments covariance predicted: " << std::endl << moments_cov << std::endl << std::endl;
 	std::cout << "Deviation : " << std::endl << (cov_moments_mc - moments_cov)/cov_moments_mc * 100 << " %" << std::endl << std::endl;
+
+
+	std::cout << "######***** Lambda_I *****######"<< std::endl;
+
+	std::cout << "Mean Lambda_I : " << results_lambda_I_mean.t() << std::endl;
+	std::cout << "Lambda_I covariance from MC: " << std::endl << cov_lambda_I_mc << std::endl;
+	std::cout << "Lambda_I covariance predicted: " << std::endl << lambda_I_cov.t() << std::endl << std::endl;
+	std::cout << "Deviation : " << std::endl << (cov_lambda_I_mc - lambda_I_cov.t())/cov_lambda_I_mc * 100 << " %" << std::endl << std::endl;
+
+
+	std::cout << "######***** Evectors *****######"<< std::endl;
+
+	std::cout << "Mean Evectors : " << results_Evectors_mean.t() << std::endl;
+	std::cout << "Evectors covariance from MC: " << std::endl << cov_Evectors_mc << std::endl;
+	std::cout << "Evectors covariance predicted: " << std::endl << Evectors_cov << std::endl << std::endl;
+	std::cout << "Deviation : " << std::endl << (cov_Evectors_mc - Evectors_cov)/cov_Evectors_mc * 100 << " %" << std::endl << std::endl;
+
+
+
+	std::cout << "######***** Eigenvectors *****######"<< std::endl;
+
+	std::cout << "Mean Eigenvectors : " << results_eigenvectors_mean.t() << std::endl;
+	std::cout << "Eigenvectors covariance from MC: " << std::endl << cov_eigenvectors_mc << std::endl;
+	std::cout << "Eigenvectors covariance predicted: " << std::endl << eigenvectors_cov << std::endl << std::endl;
+	std::cout << "Deviation : " << std::endl << (cov_eigenvectors_mc - eigenvectors_cov)/cov_eigenvectors_mc * 100 << " %" << std::endl << std::endl;
+
+
+	std::cout << "######***** MRP *****######"<< std::endl;
+
+	std::cout << "Mean MRP : " << results_mrp_mean.t() << std::endl;
+	std::cout << "MRP covariance from MC: " << std::endl << cov_mrp_mc << std::endl;
+	std::cout << "MRP covariance predicted: " << std::endl << mrp_cov << std::endl << std::endl;
+	std::cout << "Deviation : " << std::endl << (cov_mrp_mc - mrp_cov)/cov_mrp_mc * 100 << " %" << std::endl << std::endl;
 
 
 
