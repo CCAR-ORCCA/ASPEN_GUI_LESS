4d43adcbc5c85d5af37069c1afc1da8f235bfafe
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-09-05 10:40:39 -0600

    disabling rotational noise

--- Apps/IOD/main.cpp
+++ Apps/IOD/main.cpp
@@ -57,7 +57,7 @@
 
 // Rigid transform artificial noise
 #define RIGID_TRANSFORM_X_SD 0.0001
-#define RIGID_TRANSFORM_SIGMA_SD 0.0001
+#define RIGID_TRANSFORM_SIGMA_SD 0.000
 
 #define USE_BA false // Whether or not the bundle adjustment should be used
 #define USE_ICP false // Whether or not the ICP should be used (if not, uses true rigid transforms)
