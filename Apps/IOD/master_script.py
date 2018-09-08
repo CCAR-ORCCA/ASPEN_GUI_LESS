f212261c6eb6fb0a72bb3cc2e8929644e3656d8e
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-09-07 22:57:04 -0600

    starting close to apogee

--- Apps/IOD/master_script.py
+++ Apps/IOD/master_script.py
@@ -3,29 +3,29 @@ import json
 
 
 all_data = [{"ORBIT_FRACTION" : 0.25,"OBSERVATION_TIMES" : 5,"SMA" : 1000,"E" : 0.25,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_0"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_0"},
 {"ORBIT_FRACTION" : 0.25,"OBSERVATION_TIMES" : 7,"SMA" : 1000,"E" : 0.25,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_1"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_1"},
 {"ORBIT_FRACTION" : 0.25,"OBSERVATION_TIMES" : 10,"SMA" : 1000,"E" : 0.25,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_2"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_2"},
 {"ORBIT_FRACTION" : 0.5,"OBSERVATION_TIMES" : 5,"SMA" : 1000,"E" : 0.25,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_3"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_3"},
 {"ORBIT_FRACTION" : 0.5,"OBSERVATION_TIMES" : 7,"SMA" : 1000,"E" : 0.25,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_4"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_4"},
 {"ORBIT_FRACTION" : 0.5,"OBSERVATION_TIMES" : 10,"SMA" : 1000,"E" : 0.25,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_5"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_5"},
 {"ORBIT_FRACTION" : 0.25,"OBSERVATION_TIMES" : 5,"SMA" : 1000,"E" : 0.75,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_6"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_6"},
 {"ORBIT_FRACTION" : 0.25,"OBSERVATION_TIMES" : 7,"SMA" : 1000,"E" : 0.75,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_7"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_7"},
 {"ORBIT_FRACTION" : 0.25,"OBSERVATION_TIMES" : 10,"SMA" : 1000,"E" : 0.75,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_8"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_8"},
 {"ORBIT_FRACTION" : 0.5,"OBSERVATION_TIMES" : 5,"SMA" : 1000,"E" : 0.75,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_9"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_9"},
 {"ORBIT_FRACTION" : 0.5,"OBSERVATION_TIMES" : 7,"SMA" : 1000,"E" : 0.75,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_10"},
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_10"},
 {"ORBIT_FRACTION" : 0.5,"OBSERVATION_TIMES" : 10,"SMA" : 1000,"E" : 0.75,
-"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 1.6,"dir" : "../output/data_11"}]
+"I" : 1.4,"RAAN" :0.2,"PERI_OMEGA" : 0.3,"M0" : 3.,"dir" : "../output/data_11"}]
 
 
 
