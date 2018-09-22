90de5ad2deded4a7b7743c6241f9c8bb7d7f2d6a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-09-22 12:40:56 -0600

    removing omp print

--- lib/source/EstimateNormals.cpp
+++ lib/source/EstimateNormals.cpp
@@ -14,15 +14,9 @@ void EstimateNormals<PointCloud<PointNormal> >::estimate_normals(int N_neighbors
 
 
 
-
-
-
-
-
-
 	#pragma omp parallel for 
 	for (unsigned int i = 0; i < this -> pc .  size(); ++i) {
-		std::cout << omp_get_num_threads() << std::endl;
+
 		// Get the N nearest neighbors to this point
 		auto closest_points = this -> pc.get_closest_N_points(this -> pc.get_point_coordinates(i), N_neighbors);
 
