3b4067acf296012cb6e117e3b07f3dbfaf02139f
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-05 17:25:35 -0600

    got curve closing right, now working on shape closure

--- lib/include/BSpline.hpp
+++ lib/include/BSpline.hpp
@@ -11,44 +11,47 @@ public:
 
 	/**
 	Constructor. Will initialize the BSpline as a unit box
-	@param l number of unique points per direction
-	@param p degree
+	@param n_u n_u + 1 == number of basis functions along u direction
+	@param n_v n_v + 1 == number of basis functions along v direction
+	@param p_u degree of basis functions in u direction
+	@param p_v degree of basis functions in v direction
+	@param knots_u vector of u knots (if empty, will be made uniform over [0,1])
+	@param knots_v vector of v knots (if empty, will be made uniform over [0,1])
 	*/
-	BSpline(int l,int p);
-
+	BSpline(int n_u,int n_v,int p_u,int p_v,
+		std::vector<double> knots_u = {},
+		std::vector<double> knots_v = {},
+		int type = 0);
 
 	/**
-	Evaluates the (i,j) basis function
+	Evaluates the (i,p) basis function
 	@param t coordinate 0 <= t <= 1
-	@param i first index of basis function ( 0 <= i <= N - 1) , N == number of control points
-	@param j second index of basis function ( 0 <= j <= N - 1) , N == number of control points
+	@param i index of basis function ( 0 <= i <= N - 1) , N == number of control points
+	@param p degree of basis function
 	@param knots vector of knots (should have length of N + 1 - degree)
 	@return computed basis function at t
 	*/
-	static double basis_function( double t, int i, int j,const std::vector<double> & knots);
+	static double basis_function( double t, int i, int p,const std::vector<double> & knots);
 	
 	/**
 	Evaluates the BSpline
-	@param t first coordinate 0 <= t <= 1
-	@param u second coordinate 0 <= t <= 1
-	@return computed BSpline at (t,u)
+	@param u first coordinate 0 <= t <= 1
+	@param v second coordinate 0 <= t <= 1
+	@return computed BSpline at (u,v)
 	*/
-	arma::vec::fixed<3> evaluate(double t,double u) const;
-
-	/**
-	Get i-th knot
-	@param i index of knot
-	@param return knot
-	*/
-	double get_knot(int i ) const;
+	arma::vec::fixed<3> evaluate(double u,double v) const;
 
 
 	/**
-	Get domain over which the surface is defined
-	@param[out] u_min lower bound of domain
-	@param[out] u_max upper bound of domain
+	Get BSpline domain
+	@param[out] u_min minumum of u
+	@param[out] v_min minumum of v
+	@param[out] u_max maximum of u
+	@param[out] v_max maximum of v
 	*/
-	void get_domain(double & u_min,double & u_max) const;
+	void get_domain(double & u_min,double & v_min,double & u_max,double & v_max) const;
+
+
 
 	/**
 	Saves control mesh at the provided location
@@ -56,19 +59,42 @@ public:
 	void 	save_control_mesh(std::string partial) const;
 
 
+	enum Type{
+		Opened,Clamped,Closed
+	};
+
+
 
 private:
 
-	double basis_function( double t, int i, int j) const;
+
+	/**
+	Determines whether the provided direction is open or clamped
+	@param[out] is_open_at_start true if curve is open at beginning
+	@param[out] is_open_at_end true if curve is open at end
+	@param[in] p curve degree
+	@param[in] knots curve knots
+	*/
+	static void check_if_clamped(bool & is_open_at_start,bool & is_open_at_end,
+		int p,const std::vector<double> & knots);
 
 	std::vector<ControlPoint> control_points;
 	std::vector<std::vector<int> > control_point_indices;
 
-	std::vector<double> knots;
+	std::vector<double> knots_u;
+	std::vector<double> knots_v;
 
-	int n;
-	int p;
-	int m;
+	int n_u;
+	int n_v;
+
+	int p_u;
+	int p_v;
+
+	int m_u;
+	int m_v;
+
+	int type;
+
 
 
 };
--- lib/source/BSpline.cpp
+++ lib/source/BSpline.cpp
@@ -6,105 +6,151 @@
 #define BSPLINE_DEBUG 1
 
 
-BSpline::BSpline(int l,int p){
+BSpline::BSpline(int n_u,int n_v,int p_u,int p_v,
+	std::vector<double> knots_u,
+	std::vector<double> knots_v,
+	int type){
 
-	// Setting internals
-	this -> p = p;
-
-	this -> m = l + 2 * p;
-
-	this -> n = l + p - 1;
-
-	#if BSPLINE_DEBUG
-	std::cout << "Degree (p): " << this -> p << std::endl;
-	std::cout << "Number of base functions (n+1): " << this -> n + 1 << std::endl;
-	std::cout << "Number of knots (m+1): " << this -> m + 1 << std::endl;
-	#endif
-
-	// Resizing the various containers
-	this -> knots.resize(this -> m + 1);;
+	this -> type = type;
 	
-	this -> control_point_indices.resize(this -> n + 1);
-	for (std::vector<int> & indices : this -> control_point_indices){
-		indices.resize(this -> control_point_indices.size());
-	}
+	// Setting degrees
+	this -> p_u = p_u;
+	this -> p_v = p_v;
 
-	// Creating the connectivity table 
-	for (int i = 0 ; i < this -> control_point_indices.size(); ++i){
-		for(int j = 0; j < this -> control_point_indices.size(); ++j){
+	// Settings number of basis functions
+	this -> n_u = n_u;
+	this -> n_v = n_v;
 
-			int i_before_wrapping = i;
-			int j_before_wrapping = j;
+	// Setting knots
+	this -> knots_u = knots_u;
+	this -> knots_v = knots_v;
 
-			// Wrapping endpoints to close
-			bool wrapping = false;
-			if (i > this -> n - this -> p){
-				i_before_wrapping -= (this -> n - this -> p + 1);
-				wrapping = true;
-			}
-
-			// Wrapping endpoints to close
-			if (j > this -> n - this -> p){
-				j_before_wrapping -= (this -> n - this -> p + 1);
-				wrapping = true;
-			}
-
-			int index = l * i_before_wrapping + j_before_wrapping;
-
-			this -> control_point_indices[i][j] = index;
-
-			
-
-
-			if (!wrapping){
-
-				// Inserting the new point in the mesh
-				double phi = ( arma::datum::pi * static_cast<double>(i) / l);
-				double theta = (2 * arma::datum::pi * static_cast<double>(j ) / l);
-
-
-				std::cout << "phi : " << phi << " , theta : "  << theta << std::endl;
-
-
-				arma::vec::fixed<3> coords = {
-					std::cos(phi),
-					double(j),
-					std::sin(phi)
-				};
-
-				ControlPoint new_point;
-				new_point.set_point_coordinates(coords);
-				this -> control_points.push_back(new_point);
-			}
-
-			#if BSPLINE_DEBUG
-			std::cout << "(i,j): (" << i<< "," << j << "), index = " << index ;
-			if (!wrapping){
-				std::cout << this -> control_points.back().get_point_coordinates().t();
-			}
-			else{
-				std::cout << std::endl;
-			}
-			#endif
-
+	// Resizing the knots containers and generating uniform knots
+	// if no knots were provided
+	if (knots_u.size() == 0 || type == BSpline::Type::Closed){
+		this -> m_u = this -> n_u + this -> p_u + 1;
+		this -> knots_u.resize(this -> m_u + 1);
+		for (int i = 0; i < this -> knots_u.size(); ++i){
+			this -> knots_u[i] = static_cast<double>(i)/(this -> knots_u.size() - 1);
 		}
 	}
-	assert(this -> control_points.size() == l * l);
-
-	// The uniform knots are generated
-	for (int i = 0; i < this -> knots.size(); ++i){
-		this -> knots[i] = static_cast<double>(i)/(this -> knots.size() - 1);
+	else{
+		this -> m_u = static_cast<int>(knots_u.size() ) - 1;
 	}
 
-	// Creating the control points. 
+	if (knots_v.size() == 0 || type == BSpline::Type::Closed){
+		this -> m_v = this -> n_v + this -> p_v + 1;
+		this -> knots_v.resize(this -> m_v + 1);
+		for (int i = 0; i < this -> knots_v.size(); ++i){
+			this -> knots_v[i] = static_cast<double>(i)/(this -> knots_v.size() - 1);
+		}
+	}
+	else{
+		this -> m_v = static_cast<int>(knots_v.size() ) - 1;
+
+	}
+
+	this -> control_point_indices.resize(this -> n_u + 1);
+
+	for (std::vector<int> & indices : this -> control_point_indices){
+		indices.resize(this -> n_v + 1);
+	}
+
+
+
+	if (type != BSpline::Type::Closed){
+		
+		// Creating the connectivity table 
+		for (int i = 0 ; i < this -> n_u + 1; ++i){
+			for(int j = 0; j < this -> n_v + 1; ++j){
+
+				ControlPoint new_point;
+
+				arma::vec::fixed<3> coords = {
+					static_cast<double>(j),
+					static_cast<double>(i),
+					1 - std::sqrt( std::pow(i - this -> n_u/2.,2) + std::pow(j - this -> n_v/2.,2))
+				};
+
+				new_point.set_point_coordinates(coords);
+				this -> control_point_indices[i][j] = this -> control_points.size();
+				this -> control_points.push_back(new_point);
+
+
+			}
+		}
+	}
+	else{
+
+		// Creating the connectivity table 
+		for (int i = 0 ; i < this -> n_u + 1; ++i){
+			for(int j = 0; j < this -> n_v + 1; ++j){
+
+				int i_before_wrapping = i;
+				int j_before_wrapping = j;
+
+			// Wrapping endpoints to close
+				bool wrapping_u = false;
+				if (i > this -> n_u - this -> p_u){
+					i_before_wrapping -= (this -> n_u - this -> p_u + 1);
+					wrapping_u = true;
+				}
+
+			// Wrapping endpoints to close
+				bool wrapping_v = false;
+
+				if (j == this -> n){
+					// j_before_wrapping -= (this -> n - this -> p + 1);
+					// wrapping = true;
+				}
+
+				if (wrapping_u){
+					int index = (this -> n_v + 1) * i_before_wrapping + j_before_wrapping;
+					this -> control_point_indices[i][j] = index;
+
+				}
+				else if (wrapping_v){
+
+					// int index = (this -> n_v + 1) * i_before_wrapping + j_before_wrapping;
+					// this -> control_point_indices[i][j] = index;
+
+				}
+				else{
+
+					ControlPoint new_point;
+
+					arma::vec::fixed<3> coords = {
+						static_cast<double>(j),
+						static_cast<double>(i),
+						1 - std::sqrt( std::pow(i - this -> n_u/2.,2) + std::pow(j - this -> n_v/2.,2))
+					};
+
+					new_point.set_point_coordinates(coords);
+					this -> control_point_indices[i][j] = this -> control_points.size();
+					this -> control_points.push_back(new_point);
+				}
+
+			}
+		}
+
+
+	}
 	
 	
 }
 
 
-double BSpline::basis_function(double t, int i,  int j,const std::vector<double> & knots){
 
-	if (j == 0){
+
+
+
+
+
+
+double BSpline::basis_function(double t, int i,  int p,const std::vector<double> & knots){
+
+
+	if (p == 0){
 		if ( (knots[i] <= t) && (t < knots[i + 1])){
 			return 1.;
 		}
@@ -114,39 +160,45 @@ double BSpline::basis_function(double t, int i,  int j,const std::vector<double>
 	}
 	else{
 		
+
+
 		double a,b;
-		if (knots[i + j] != knots[i]){
-			a = (t - knots[i])/(knots[i + j] - knots[i]);
+
+		if (knots[i + p] != knots[i]){
+			a = (t - knots[i])/(knots[i + p] - knots[i]);
 		}
+
 		else{
 			a = 0.;
 		}
-		if (knots[i + j + 1] != knots[i + 1]){
-			b = ( knots[i + j + 1] - t)/(knots[i + j + 1] - knots[i + 1]);
+
+		if (knots[i + p + 1] != knots[i + 1]){
+			b = ( knots[i + p + 1] - t)/(knots[i + p + 1] - knots[i + 1]);
 		}
+
 		else{
 			b = 0.;
 		}
 
-		return ( a * BSpline::basis_function(t, i,j - 1,knots) 
-			+ b * BSpline::basis_function(t, i + 1,j - 1,knots));
+
+		return ( a * BSpline::basis_function(t, i,p - 1,knots) 
+			+ b * BSpline::basis_function(t, i + 1,p - 1,knots));
 	}
 
 
 }
 
-double BSpline::basis_function(double t, int i,  int j) const{
-	return BSpline::basis_function(t,i,j,this -> knots);
-}
 
 
-arma::vec::fixed<3> BSpline::evaluate(double t,double u) const{
+arma::vec::fixed<3> BSpline::evaluate(double u,double v) const{
 
 	arma::vec::fixed<3> P = {0,0,0};
-	for (int i = 0; i <= this -> n ; ++i){
-		for (int j = 0; j <= this -> n ; ++j){
-			P += (this -> basis_function(t,i,this -> p) 
-				* this -> basis_function(u,j,this -> p)
+
+	for (int i = 0; i < this -> n_u  + 1; ++i){
+		for (int j = 0; j < this -> n_v  + 1; ++j){
+
+			P += (BSpline::basis_function(u,i,this -> p_u,this -> knots_u) 
+				* BSpline::basis_function(v,j,this -> p_v,this -> knots_v)
 				* this -> control_points[this -> control_point_indices[i][j]].get_point_coordinates());
 		}
 	}
@@ -156,18 +208,90 @@ arma::vec::fixed<3> BSpline::evaluate(double t,double u) const{
 }
 
 
-double BSpline::get_knot(int i ) const{
-	return this -> knots[i];
+
+
+void BSpline::check_if_clamped(bool & is_open_at_start,bool & is_open_at_end,
+	int p,const std::vector<double> & knots){
+
+	is_open_at_start = false;
+	for (int i = 0; i < p + 1; ++i){
+		if (knots[i] != knots[0]){
+			is_open_at_start = true;
+			break;
+		}
+	}
+
+	is_open_at_end = false;
+	for (int i = 0; i < p + 1; ++i){
+		if (knots[static_cast<int>(knots.size()) - 1 - i] != knots.back()){
+			is_open_at_end = true;
+
+			break;
+		}
+	}
+
 }
 
+void BSpline::get_domain(double & u_min,double & v_min,double & u_max,double & v_max) const{
 
-void BSpline::get_domain(double & u_min,double & u_max) const{
-	
-	u_min = this -> knots[this -> p];
-	u_max = this -> knots[this -> n - this -> p];
+
+	if (this -> type == BSpline::Type::Closed){
+
+		u_min = this -> knots_u[this -> p_u];
+		u_max = this -> knots_u[this -> m_u - this -> p_u];
+
+		bool is_open_at_start,is_open_at_end;
+
+		BSpline::check_if_clamped(is_open_at_start,is_open_at_end,this -> p_v,this -> knots_v);
+		if (is_open_at_start){
+			v_min = this -> knots_v[this -> p_v];
+		}
+		else{
+			v_min = 0;
+		}
+		if (is_open_at_end){
+			v_max = this -> knots_v[this -> m_v - this -> p_v];
+		}
+		else{
+			v_max = 1;
+		}
+
+	}
+	else{
+	// u
+		bool is_open_at_start,is_open_at_end;
+		BSpline::check_if_clamped(is_open_at_start,is_open_at_end,this -> p_u,this -> knots_u);
+
+		if (is_open_at_start){
+			u_min = this -> knots_u[this -> p_u];
+		}
+		else{
+			u_min = 0;
+		}
+		if (is_open_at_end){
+			u_max = this -> knots_u[this -> m_u - this -> p_u];
+		}
+		else{
+			u_max = 1;
+		}
+
+		BSpline::check_if_clamped(is_open_at_start,is_open_at_end,this -> p_v,this -> knots_v);
+		if (is_open_at_start){
+			v_min = this -> knots_v[this -> p_v];
+		}
+		else{
+			v_min = 0;
+		}
+		if (is_open_at_end){
+			v_max = this -> knots_v[this -> m_v - this -> p_v];
+		}
+		else{
+			v_max = 1;
+		}
+	}
+
 
 }
-
 
 void BSpline::save_control_mesh(std::string partial_path) const{
 
