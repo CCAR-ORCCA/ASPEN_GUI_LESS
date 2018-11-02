27fcb744efa66f5fdda1518cbc568b2972a945de
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-11-01 18:43:11 -0600

    debug

--- lib/source/ShapeBuilder.cpp
+++ lib/source/ShapeBuilder.cpp
@@ -191,7 +191,7 @@ void ShapeBuilder::run_shape_reconstruction(const arma::vec &times ,
 			if (this -> filter_arguments -> get_use_ba()){
 				ba_test.run(M_pcs,X_pcs,BN_measured,mrps_LN,false);
 			}
-			std::cout << "True state at epoch time before running IOD: " << X[epoch_time_index].subvec(0,5).t() << std::endl;
+			std::cout << "True state at epoch time at index "<< epoch_time_index << " before running IOD: " << X[epoch_time_index].subvec(0,5).t() << std::endl;
 			this -> run_IOD_finder(times, epoch_time_index ,time_index, mrps_LN,X_pcs,M_pcs,R_pcs,iod_state);
 
 			// Updating the epoch time here so that things remain consistent
