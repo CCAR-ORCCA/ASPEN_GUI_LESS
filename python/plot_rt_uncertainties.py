9a4ab065d924e6215118fb55ea63475d485292f0
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-11-04 14:12:10 -0700

    more obs

--- python/plot_rt_uncertainties.py
+++ python/plot_rt_uncertainties.py
@@ -10,23 +10,55 @@ mrp_errors = np.loadtxt(path_to_folder + "/mrp_error_arma.txt")
 R = np.loadtxt(path_to_folder + "/R_pcs_arma.txt")
 
 
-
 plt.scatter(X_errors[:,1],X_errors[:,2],c = range(len(X_errors)))
 for i in range(len(X_errors) - 1):
 	draw_2d_covariance(0,R[i,:].reshape(6,6)[0:3,0:3],'b')
-
-
-
+plt.title("X uncertainty, projected along $e_x$")
 plt.axis("equal")
 plt.show()
 
+plt.clf()
+plt.scatter(X_errors[:,0],X_errors[:,2],c = range(len(X_errors)))
+for i in range(len(X_errors) - 1):
+	draw_2d_covariance(1,R[i,:].reshape(6,6)[0:3,0:3],'b')
+plt.title("X uncertainty, projected along $e_y$")
+plt.axis("equal")
+plt.show()
+
+plt.clf()
+
+plt.scatter(X_errors[:,0],X_errors[:,1],c = range(len(X_errors)))
+for i in range(len(X_errors) - 1):
+	draw_2d_covariance(2,R[i,:].reshape(6,6)[0:3,0:3],'b')
+plt.title("X uncertainty, projected along $e_z$")
+plt.axis("equal")
+plt.show()
+
+plt.clf()
 
 plt.scatter(mrp_errors[:,0],mrp_errors[:,1],c = range(len(mrp_errors)))
 for i in range(len(mrp_errors) - 1):
 	draw_2d_covariance(2,R[i,:].reshape(6,6)[3:6,3:6],'b')
 
+plt.axis("equal")
+plt.show()
 
 
+plt.clf()
+
+plt.scatter(mrp_errors[:,0],mrp_errors[:,2],c = range(len(mrp_errors)))
+for i in range(len(mrp_errors) - 1):
+	draw_2d_covariance(1,R[i,:].reshape(6,6)[3:6,3:6],'b')
+
+plt.axis("equal")
+plt.show()
+
+
+plt.clf()
+
+plt.scatter(mrp_errors[:,1],mrp_errors[:,2],c = range(len(mrp_errors)))
+for i in range(len(mrp_errors) - 1):
+	draw_2d_covariance(0,R[i,:].reshape(6,6)[3:6,3:6],'b')
 
 plt.axis("equal")
 plt.show()
