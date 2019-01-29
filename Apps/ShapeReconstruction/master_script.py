e02c52726cfa746ed6a87f48c778340615463852
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-01-28 23:51:26 -0700

    corrected master_script.py

--- Apps/ShapeReconstruction/master_script.py
+++ Apps/ShapeReconstruction/master_script.py
@@ -28,87 +28,50 @@ else:
     base_location = "/Users/bbercovici/GDrive/CUBoulder/Research/code/ASPEN_gui_less/Apps/"
 
 
-# base_dictionnary = {
-# "CR_TRUTH" : 1.2,
-# "USE_BA" : True,
-# "USE_ICP" : True,
-# "USE_TRUE_RIGID_TRANSFORMS" : False,
-# "MIN_TRIANGLE_ANGLE" : 30,
-# "MAX_TRIANGLE_SIZE" : 5,
-# "HARMONICS_DEGREE" : 10,
-# "USE_HARMONICS" : True,
-# "TF" : 96.,
-# "SMA" : 1000.,
-# "E" : 0.25,
-# "I" : 1.4,
-# "RAAN"  : 0.2,
-# "PERI_OMEGA" : 0.3,
-# "M0" : 1.57,
-# "SPIN_PERIOD" : 12.,
-# "LONGITUDE_SPIN" : 0., 
-# "LATITUDE_SPIN" : 0.,
-# "DENSITY" : 1900,
-# "MRP_0" : [0,0,0],
-# "SURFACE_APPROX_ERROR" : 1,
-# "BA_H" : 6,
-# "LOS_NOISE_SD_BASELINE" : 5e-1,
-# "N_ITER_BUNDLE_ADJUSTMENT" : 3,
-# "N_ITER_SHAPE_FILTER" : 3,
-# "IOD_ITERATIONS" : 100,
-# "IOD_RIGID_TRANSFORMS_NUMBER" : 7,
-# "IOD_PARTICLES" : 100,
-# "USE_BEZIER_SHAPE" : False,
-# "USE_TARGET_POI" : False
-# }
+base_dictionnary = {
+"CR_TRUTH" : 1.2,
+"USE_BA" : True,
+"USE_ICP" : True,
+"USE_TRUE_RIGID_TRANSFORMS" : False,
+"MIN_TRIANGLE_ANGLE" : 30,
+"MAX_TRIANGLE_SIZE" : 5,
+"HARMONICS_DEGREE" : 10,
+"USE_HARMONICS" : True,
+"TF" : 96.,
+"SMA" : 1000.,
+"E" : 0.25,
+"I" : 1.4,
+"RAAN"  : 0.2,
+"PERI_OMEGA" : 0.3,
+"M0" : 1.57,
+"SPIN_PERIOD" : 12.,
+"LONGITUDE_SPIN" : 0., 
+"LATITUDE_SPIN" : 0.,
+"DENSITY" : 1900,
+"MRP_0" : [0,0,0],
+"SURFACE_APPROX_ERROR" : 1,
+"BA_H" : 6,
+"LOS_NOISE_SD_BASELINE" : 5e-1,
+"N_ITER_BUNDLE_ADJUSTMENT" : 3,
+"N_ITER_SHAPE_FILTER" : 3,
+"IOD_ITERATIONS" : 100,
+"IOD_RIGID_TRANSFORMS_NUMBER" : 7,
+"IOD_PARTICLES" : 100,
+"USE_BEZIER_SHAPE" : False,
+"USE_TARGET_POI" : False
+}
 
 
-# all_cases_dictionnary = {
-# "USE_BEZIER_SHAPE" : [False,True],
-# "INSTRUMENT_FREQUENCY_SHAPE" : [0.0004,0.0005,0.0006],
-# "NUMBER_OF_EDGES" : [1500,2000,2500,3000]
-# }
+all_cases_dictionnary = {
+"USE_BEZIER_SHAPE" : [False,True],
+"INSTRUMENT_FREQUENCY_SHAPE" : [0.0004,0.0005,0.0006],
+"NUMBER_OF_EDGES" : [1500,2000,2500,3000]
+}
 
 
-# all_data = generate_all_cases_dictionnary_list(base_dictionnary,
-#     all_cases_dictionnary,base_location)
+all_data = generate_all_cases_dictionnary_list(base_dictionnary,
+    all_cases_dictionnary,base_location)
 
-all_data = [ {
-    "CR_TRUTH" : 1.2,
-    "USE_BA" : True,
-    "USE_ICP" : True,
-    "USE_TRUE_RIGID_TRANSFORMS" : False,
-    "MIN_TRIANGLE_ANGLE" : 30,
-    "MAX_TRIANGLE_SIZE" : 5,
-    "HARMONICS_DEGREE" : 10,
-    "USE_HARMONICS" : True,
-    "TF" : 96.,
-    "SMA" : 1000.,
-    "E" : 0.25,
-    "I" : 1.4,
-    "RAAN"  : 0.2,
-    "PERI_OMEGA" : 0.3,
-    "M0" : 1.57,
-    "SPIN_PERIOD" : 12.,
-    "LONGITUDE_SPIN" : 0., 
-    "LATITUDE_SPIN" : 0.,
-    "DENSITY" : 1900,
-    "MRP_0" : [0,0,0],
-    "SURFACE_APPROX_ERROR" : 1,
-    "BA_H" : 6,
-    "LOS_NOISE_SD_BASELINE" : 5e-1,
-    "N_ITER_BUNDLE_ADJUSTMENT" : 2,
-    "N_ITER_SHAPE_FILTER" : 3,
-    "IOD_ITERATIONS" : 100,
-    "IOD_RIGID_TRANSFORMS_NUMBER" : 7,
-    "IOD_PARTICLES" : 100,
-    "USE_BEZIER_SHAPE" : False,
-    "USE_TARGET_POI" : False,
-    "USE_BEZIER_SHAPE" : False,
-    "INSTRUMENT_FREQUENCY_SHAPE" : 0.0004,
-    "NUMBER_OF_EDGES" : 1500,
-    "INPUT_DIR" : base_location + "ShapeReconstruction/input/case_99",
-    "OUTPUT_DIR" : base_location + "ShapeReconstruction/output/case_99"
-    }]
 
 
 for data in all_data:
