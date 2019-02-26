7a69ada0a9f9f71c725a358a925a480a13ecdf9c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-25 19:31:09 -0700

    allowing non-consecutive edges to be removed

--- lib/source/BundleAdjuster.cpp
+++ lib/source/BundleAdjuster.cpp
@@ -1087,6 +1087,7 @@ bool BundleAdjuster::can_remove_edge(const std::set<int> & edge_to_remove) {
 	bool p0_surrounded = (first_pc_neighbors.lower_bound(p0) != first_pc_neighbors.end()) && (first_pc_neighbors.upper_bound(p0) != first_pc_neighbors.end());
 	bool p1_surrounded = (second_pc_neighbors.lower_bound(p1) != second_pc_neighbors.end()) && (second_pc_neighbors.upper_bound(p1) != second_pc_neighbors.end());
 
+	std::cout << p0_surrounded << " | " << p1_surrounded << std::endl;
 
 	return (p0_surrounded && p1_surrounded);
 
